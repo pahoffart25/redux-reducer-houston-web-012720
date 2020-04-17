015ab4805fb0328035aa0489bbe8a1c3cb22bd6c
pahoffart25 <pahoffart25@gmail.com>
2020-04-17 09:57:55 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            state.friends.push(action.friend)
+            return {friends: state.friends}
+        case "REMOVE_FRIEND":
+            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+        default:
+            return state
+    }
 }
+
+let state = {friends: []}
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+    switch(action.type){
+        case "INCREASE":
+            return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state
+    }
 }
+
+let state = {numberOfPresents: 0}
+let action = {type: "INCREASE"}
